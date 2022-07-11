c199391787f9c81f2d89fa6d74e9bbbaa2610f3f
missmahi55 <mahakagarwal248@gmail.com>
2022-07-11 17:37:23 +0530

    home and card components created

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './Card/Card';
+import './Home.css';
+
+function Home() {
+  return (
+    <div>
+        <Card/>
+    </div>
+  )
+}
+
+export default Home
