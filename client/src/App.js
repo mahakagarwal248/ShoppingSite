e5e8a77ad95d1aa3ea65cd581a8d7974ddad33fb
missmahi55 <mahakagarwal248@gmail.com>
2022-07-11 16:40:18 +0530

    added navbar and foote to appr

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      hey
+      <Navbar/>
+      <Footer/>
     </div>
   );
 }
