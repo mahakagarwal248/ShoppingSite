bcf99790be4438a35b85f030c03e6c47d11dedcd
missmahi55 <mahakagarwal248@gmail.com>
2022-07-11 16:52:16 +0530

    navbar updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import {BrowserRouter, Routes , Route } from 'react-router-dom'
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={
+          <div className="App">
+          <Navbar/>
+          <Footer/>
+        </div>
+        }/>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import './Navbar.css'
+import './Navbar.css';
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <Link to="/" className='nav-items'>
+            Home
+        </Link>
+        <Link to="/" className='nav-items'>
+            About
+        </Link>
+        <Link to="/" className='nav-items'>
+            Login
+        </Link>
+    </div>
   )
 }
 
