19f92684a8f9ad0292d08358b9480b24ec885bc9
missmahi55 <mahakagarwal248@gmail.com>
2022-07-11 23:20:49 +0530

    Login form created

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -5,6 +5,19 @@ function Login() {
   return (
     <div className='login-container'>
         <h1>Welcome to the <i>Shopping Site</i></h1>
+        <div style={{marginTop:'25px'}}>
+          <button>Sign up</button>
+          <p>or</p>
+          <form className='login-form'>
+            <label>Email</label><br/>
+            <input type="email" placeholder="Enter email"/>
+            <br/>
+            <label>Password</label><br/>
+            <input type="password" placeholder="Enter your password"/>
+            <br/>
+            <button type='submit' className='login-form-btn'>Login</button>
+          </form>
+        </div>
     </div>
   )
 }
