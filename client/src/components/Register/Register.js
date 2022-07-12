8d439004783bdd467204c7ebb4528e6188a29488
missmahi55 <mahakagarwal248@gmail.com>
2022-07-12 10:41:52 +0530

    User register form created

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import './Register.css'
+import React from "react";
+import "./Register.css";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="register-container">
+      <form className="register-form">
+        <label>Name</label>
+        <br />
+        <input type="name" placeholder="Enter name" />
+        <br />
+        <label>Email</label>
+        <br />
+        <input type="email" placeholder="Enter email" />
+        <br />
+        <label>Password</label>
+        <br />
+        <input type="password" placeholder="Enter your password" />
+        <br />
+        <label>Mobile</label>
+        <br />
+        <input type="tel" placeholder="Enter mobile number" />
+        <br />
+        <label>Address</label>
+        <br />
+        <input type="text" placeholder="Enter address" />
+        <br />
+        <button type="submit" className="register-form-btn">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
