574e00bf9e9a90ba1c25eb473f0d854a5528336d
missmahi55 <mahakagarwal248@gmail.com>
2022-07-13 10:03:07 +0530

    Avatar added

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
 
 import './Navbar.css';
 
@@ -27,9 +28,10 @@ function Navbar() {
               Login
           </Link>
           :
-          <>
+          <div style={{marginLeft:'auto', display:'flex'}}>
+            <Avatar style={{height:'34px', width:'34px',color:'black'}}>H</Avatar>
             <button type='button' className='logout-btn' onClick={handleLogout}>Logout</button>
-          </>
+          </div>
         }
         
     </div>
