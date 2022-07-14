2b9a992f7a18d925d416b98365bfb824d3e3d955
missmahi55 <mahakagarwal248@gmail.com>
2022-07-14 22:00:09 +0530

    cart and wishlist icons added in navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 import './Navbar.css';
 
@@ -26,23 +28,18 @@ function Navbar() {
         <Link to="/" className='nav-items'>
             About
         </Link>
-        {
-          localStorage.getItem('Profile') !== null && 
-          <>
-            <Link to="/cart" className="nav-items">
-              Cart
-            </Link>
-            <Link to="/wishlist" className="nav-items">
-              My Wishlist
-            </Link>
-          </>
-        }
         {localStorage.getItem('Profile') === null ? 
           <Link to="/login" className='nav-items'>
               Login
           </Link>
           :
           <div style={{marginLeft:'auto', display:'flex'}}>
+            <Link to="/cart">
+              <ShoppingCartOutlinedIcon className='nav-icons'/>
+            </Link>
+            <Link to="/wishlist">
+              <FavoriteBorderOutlinedIcon className='nav-icons'/>
+            </Link>
             <Avatar style={{height:'34px', width:'34px',color:'black'}}>{User.result.name.charAt(0).toUpperCase()}</Avatar>
             <p style={{margin:'auto 70px auto 10px', fontSize:'22px'}}>Hi, {User.result.name.split(" ").slice(0, -1).join(' ')}</p>
             <button type='button' className='logout-btn' onClick={handleLogout}>Logout</button>
