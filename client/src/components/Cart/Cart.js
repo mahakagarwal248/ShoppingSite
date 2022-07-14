0651de9f05815801433421872e12ff820c051369
missmahi55 <mahakagarwal248@gmail.com>
2022-07-14 22:51:22 +0530

    cart item display table created

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,13 +1,21 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 import "./Cart.css";
 import Navbar from "../Navbar/Navbar";
-import panda from "../../assets/panda.jpg";
+// import panda from "../../assets/panda.jpg";
 import { fetchCartProduct } from "../../actions/Cart";
 import { Link } from "react-router-dom";
 
 function Cart() {
+
   const cartProductList = useSelector((state) => state.cartReducer);
   console.log(cartProductList);
 
@@ -38,24 +46,42 @@ function Cart() {
         </>
       ) : (
         <>
-          {cartProductList.data.map((products) => (
-            <div className="display-cart-items-container" key={products._id}>
-              <div>
-                <div className="display-cart-items-container-1">
-                  <img src={panda} alt="product" />
-                </div>
-                <div className="display-cart-items-container-2">
-                  <h4>{products.name}</h4>
-                  <p>Product description</p>
-                  <span>
-                    <b>INR 5000</b>
-                  </span>
-                  <br />
-                  <span>Quantity</span>
-                </div>
-              </div>
-            </div>
-          ))}
+            <TableContainer component={Paper} style={{width:'70%', margin:'auto',marginTop:'25px'}}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead style={{border:'2px solid black',backgroundColor:'darkgrey'}}>
+                <TableRow >
+                  <TableCell align="left" style={{fontSize:'17px', fontWeight:'600'}}>Name</TableCell>
+                  <TableCell align="left" style={{fontSize:'17px', fontWeight:'600'}}>Description</TableCell>
+                  <TableCell align="left" style={{fontSize:'17px', fontWeight:'600'}}>Quantity</TableCell>
+                  <TableCell align="left" style={{fontSize:'17px', fontWeight:'600'}}>Price</TableCell>
+                  <TableCell align="left" style={{fontSize:'17px', fontWeight:'600', width:'22%'}}>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody style={{border:'1px solid black'}}>
+              {cartProductList.data.map((products) => (
+                <>
+
+                  <TableRow
+                    key={products._id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    style={{borderBottom:'1px solid black'}}
+                  >
+                    <TableCell component="th" scope="row">
+                      {products.name}
+                    </TableCell>
+                    <TableCell align="left">{products.description}</TableCell>
+                    <TableCell align="left">{products.quantity}</TableCell>
+                    <TableCell align="left">{products.price}</TableCell>
+                    <TableCell align="left" style={{width:'22%'}}>
+                      <button className="cart-btn" style={{marginRight:'25px'}}>Buy Now</button>
+                      <button className="cart-btn">Delete</button>
+                    </TableCell>
+                  </TableRow>
+                  </>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </>
       )}
     </div>
