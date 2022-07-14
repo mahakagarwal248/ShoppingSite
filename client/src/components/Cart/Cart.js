394b297c3f3469d8ea20fe744c1a4b3b66b471c9
missmahi55 <mahakagarwal248@gmail.com>
2022-07-14 12:50:39 +0530

    product displaying in cart static design created

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./Cart.css";
+import Navbar from "../Navbar/Navbar";
+import panda from "../../assets/panda.jpg";
 
 function Cart() {
+  
   return (
-    <div>Cart</div>
-  )
+    <div className="cart-container">
+      <Navbar />
+      <br />
+      <h2>Welcome to your cart</h2>
+      <div className="display-cart-items-container">
+        <div>
+          <div className="display-cart-items-container-1">
+            <img src={panda} alt="product" />
+          </div>
+          <div className="display-cart-items-container-2">
+            <h4>Product Name</h4>
+            <p>Product description</p>
+            <span>
+              <b>INR 5000</b>
+            </span><br/>
+            <span>Quantity</span>
+          </div>
+        </div>
+        <div>
+          <div className="display-cart-items-container-1">
+            <img src={panda} alt="product" />
+          </div>
+          <div className="display-cart-items-container-2">
+            <h4>Product Name</h4>
+            <p>Product description</p>
+            <span>
+              <b>INR 5000</b>
+            </span><br/>
+            <span>Quantity</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
