0dbe65362acbeb8c521f5326a52e60bc5148877c
missmahi55 <mahakagarwal248@gmail.com>
2022-07-18 16:15:32 +0530

    home page css modified

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,8 +5,10 @@ import './Home.css';
 function Home() {
   return (
     <div className='home-container'>
+        <h2>Welcome to your <i>Shopping Site</i></h2>
+        <div>
         <Cards/>
-        
+        </div>
     </div>
   )
 }
