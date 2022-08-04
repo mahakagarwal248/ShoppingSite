d701ff39722d674558d4139af5528d951b47f8e8
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-08-04 16:45:00 +0530

    product image display in product details

--- client/src/components/ProductDetails/ProductDetails.js
+++ client/src/components/ProductDetails/ProductDetails.js
@@ -3,48 +3,55 @@ import { useParams } from 'react-router';
 import { useSelector } from 'react-redux';
 
 import './ProductDetails.css';
-import panda from '../../assets/panda.jpg';
 import Navbar from '../Navbar/Navbar';
 
 function ProductDetails() {
   const { id } = useParams();
   const productList = useSelector((state) => state.productReducer);
 
+  var image = {};
   return (
     <div>
       <Navbar />
       {productList?.data
         ?.filter((product) => product._id === id)
-        .map((product) => (
-          <div
-            key={product._id}
-            style={{ display: 'flex', width: '65%', margin: 'auto', marginTop: '25px' }}>
-            <div style={{ flex: '0.5' }}>
-              <img
-                src={panda}
-                alt="product"
-                style={{ height: '380px', width: '460px', marginRight: '10px' }}
-              />
-            </div>
-            <div style={{ flex: '0.5' }}>
-              <h2>{product.name}</h2>
-              <p style={{ fontSize: '20px' }}>{product.description}</p>
-              <br />
-              <p style={{ fontSize: '20px' }}>
-                <b>INR {product.price}</b>
-              </p>
-              <p>Available : {product.quantity}</p>
-              <br />
-              <br />
-              <br />
-              <div style={{ width: '200px' }}>
-                <button className="add-to-cart-btn">Add to cart</button>
-                <br />
-                <button className="add-to-wishlist-btn">Add to Wishlist</button>
+        .map(
+          (product) => (
+            (image = `data:${product.img.contentType};base64, ${buffer.Buffer.from(
+              product.img.data
+            ).toString('base64')}`),
+            (
+              <div
+                key={product._id}
+                style={{ display: 'flex', width: '65%', margin: 'auto', marginTop: '25px' }}>
+                <div style={{ flex: '0.5' }}>
+                  <img
+                    src={image}
+                    alt="product"
+                    style={{ height: '380px', width: '460px', marginRight: '10px' }}
+                  />
+                </div>
+                <div style={{ flex: '0.5' }}>
+                  <h2>{product.name}</h2>
+                  <p style={{ fontSize: '20px' }}>{product.description}</p>
+                  <br />
+                  <p style={{ fontSize: '20px' }}>
+                    <b>INR {product.price}</b>
+                  </p>
+                  <p>Available : {product.quantity}</p>
+                  <br />
+                  <br />
+                  <br />
+                  <div style={{ width: '200px' }}>
+                    <button className="add-to-cart-btn">Add to cart</button>
+                    <br />
+                    <button className="add-to-wishlist-btn">Add to Wishlist</button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
+            )
+          )
+        )}
     </div>
   );
 }
