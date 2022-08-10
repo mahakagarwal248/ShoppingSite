ae0f966f01928cba1a712f76b47073a3f4d91880
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-08-10 13:20:04 +0530

    sepration of imported files/modules in app.js modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import { useEffect } from 'react';
-import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+
+import './App.css';
+
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import Register from './components/Register/Register';
 import Cart from './components/Cart/Cart';
-import { fetchAllProducts } from './actions/Products';
 import Wishlist from './components/Wishlist/Wishlist';
 import ProductDetails from './components/ProductDetails/ProductDetails';
 import About from './components/About/About';
+
+import { fetchAllProducts } from './actions/Products';
 
 function App() {
   const dispatch = useDispatch();
