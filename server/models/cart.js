7937e8d776a594d21b1d77e7ed611a66e3655cc6
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-08-10 14:34:37 +0530

    enabled timestamps in all models

--- server/models/cart.js
+++ server/models/cart.js
@@ -1,17 +1,20 @@
 import mongoose from "mongoose";
 
-const cartSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  quantity: { type: Number, required: true },
-  brand: { type: String },
-  userId: { type: String, required: true },
-  productId: { type: String, required: true },
-  img: {
-    data: Buffer,
-    contentType: String,
+const cartSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    brand: { type: String },
+    userId: { type: String, required: true },
+    productId: { type: String, required: true },
+    img: {
+      data: Buffer,
+      contentType: String,
+    },
   },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Cart", cartSchema);
--- server/models/images.js
+++ server/models/images.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const imageSchema= mongoose.Schema({
-    img:
-    {
-        data: Buffer,
-        contentType: String
+const imageSchema = mongoose.Schema(
+  {
+    img: {
+      data: Buffer,
+      contentType: String,
     },
-    userId:{type:String, required: true}
-})
+    userId: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("images", imageSchema)
+export default mongoose.model("images", imageSchema);
