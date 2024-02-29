028352871ff83bff206273885ee39c6e9d035c3c
Mahak Agarwal <mahakagarwal248@gmail.com>
2024-02-29 16:16:14 +0530

    base url changed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API = axios.create({ baseURL: 'https://uninterested-erin-woodpecker.cyclic.app/' });
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://uninterested-erin-woodpecker.cyclic.app/' });
+// const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 export const signup = (userData) => API.post('/user/signup', userData);
 export const login = (loginData) => API.post('/user/login', loginData);
